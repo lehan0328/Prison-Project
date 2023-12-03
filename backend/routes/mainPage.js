0d8d3da148706a3f0589b14b03e9deae98cdc235
Lehan Ouyang <lo739@nyu.edu>
2023-12-03 17:29:11 -0500

    Added database query for insertions

--- backend/routes/mainPage.js
+++ backend/routes/mainPage.js
@@ -51,4 +51,110 @@ router.route('/').get((req, res) => {
       );
     })
 
+   // API endpoint to add a criminal and related information to two tables
+router.route('/add_criminal').post((req, res) => {
+  const { criminalData, criminalIdData } = req.body;
+
+  // Start a transaction
+  db.beginTransaction((err) => {
+    if (err) {
+      console.error('Error starting transaction:', err.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    // Insert into Criminal table
+    const criminalSql = 'INSERT INTO Criminal (Violent_Offender_Status, Probation_Status, Aliases) VALUES (?, ?, ?)';
+    const criminalValues = [
+      criminalData.Violent_Offender_Status,
+      criminalData.Probation_Status,
+      JSON.stringify(criminalData.Aliases),
+    ];
+
+    db.query(criminalSql, criminalValues, (criminalErr, criminalResults) => {
+      if (criminalErr) {
+        return db.rollback(() => {
+          console.error('Error adding criminal:', criminalErr.message);
+          res.status(500).json({ error: 'Internal Server Error' });
+        });
+      }
+
+      const criminalId = criminalResults.insertId;
+
+      // Insert into Criminal_ID_Table
+      const idTableSql = 'INSERT INTO Criminal_ID_Table (Criminal_ID, Phone_num, Name, Address) VALUES (?, ?, ?, ?)';
+      const idTableValues = [criminalId, criminalIdData.Phone_num, criminalIdData.Name, criminalIdData.Address];
+
+      db.query(idTableSql, idTableValues, (idTableErr) => {
+        if (idTableErr) {
+          return db.rollback(() => {
+            console.error('Error adding data to Criminal_ID_Table:', idTableErr.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+          });
+        }
+
+        // Commit the transaction if both inserts were successful
+        db.commit((commitErr) => {
+          if (commitErr) {
+            return db.rollback(() => {
+              console.error('Error committing transaction:', commitErr.message);
+              res.status(500).json({ error: 'Internal Server Error' });
+            });
+          }
+
+          console.log('Transaction committed successfully');
+          res.status(201).json({ message: 'Data added successfully' });
+        });
+      });
+    });
+  });
+});
+
+router.route('/add_criminal').post((req, res) => {
+  const { policeOfficerData } = req.body;
+
+  // Start a transaction
+  db.beginTransaction((err) => {
+    if (err) {
+      console.error('Error starting transaction:', err.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+
+    // Insert into Police_Officer table
+    const policeOfficerSql = 'INSERT INTO Police_Officer (PO_Name, Badge_num, Precinct_ID, Phone, Status) VALUES (?, ?, ?, ?, ?)';
+    const policeOfficerValues = [
+      policeOfficerData.PO_Name,
+      policeOfficerData.Badge_num,
+      policeOfficerData.Precinct_ID,
+      policeOfficerData.Phone,
+      policeOfficerData.Status,
+    ];
+
+    db.query(policeOfficerSql, policeOfficerValues, (policeOfficerErr, policeOfficerResults) => {
+      if (policeOfficerErr) {
+        return db.rollback(() => {
+          console.error('Error adding police officer:', policeOfficerErr.message);
+          res.status(500).json({ error: 'Internal Server Error' });
+        });
+      }
+
+      // Commit the transaction if the insert was successful
+      db.commit((commitErr) => {
+        if (commitErr) {
+          return db.rollback(() => {
+            console.error('Error committing transaction:', commitErr.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+          });
+        }
+
+        console.log('Transaction committed successfully');
+        res.status(201).json({ message: 'Police officer added successfully' });
+      });
+    });
+  });
+});
+
+
+
 module.exports = router;
