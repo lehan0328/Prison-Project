b1f89a8283dba3967b2c1a196d54adaea10c321e
Lehan Ouyang <lo739@nyu.edu>
2023-12-03 17:29:11 -0500

    Added database query for insertions

--- backend/server.js
+++ backend/server.js
@@ -14,21 +14,6 @@ const mainPageRouter = require('./routes/mainPage');
 
 app.use('/login', loginRouter);
 app.use('/main_page', mainPageRouter);
-// const db = mysql.createConnection({
-//     host: "database-1.cur9f5tr5hr1.us-east-2.rds.amazonaws.com",
-//     user:"admin",
-//     password:"preHoch99!",
-//     database:"prison_project"
-// })
-
-
-// app.get("/login", (req,res) => {
-//     const sql = "SELECT * FROM Appeal";
-//     db.query(sql, (err, data)=> {
-//         if(err) return res.json(err);
-//         return res.json(data);
-//     })
-// })
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
